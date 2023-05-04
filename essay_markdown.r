de73a2f19502d2c288d55ee85732d8d61018cacc
Daniel <visual.eyes.success@gmail.com>
2023-05-04 13:43:20 +0200

    create essay
    early version before deleted

---
+++ essay_markdown.r
@@ -0,0 +1,52 @@
+---
+title: "H20_markdown"
+author: "Daniel Tracy"
+date: '2023-03-18'
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+The obvious anthropogenic effects on the Global Water Cycle can be traced to many sources. Among the most caustic influential elements of global climate changes within the water resources scope are fluorinated gases such as HFC’s, PFC’s, & SF6 with such a high GWP. Global Warming Potential(GWP) refers to the energy amount which a ton of a particular atmospheric gas can absorb over a given time frame, compared to a ton of carbon dioxide(CO2). Essentially providing us with a standard of gauging how much heat a greenhouse gas can retain in the atmosphere when contrasted with CO2. This essay analyzes European Union's mitigation of Fluorinated Gases Regulation including the pros, cons, efficacy, and future horizons.
+ 
+## EU F-GASES REGULATION                  
+European Union's F-gas Regulation was adopted in 2006, and succeeded in stabilization of EU F-gas emissions at 2010 levels. The amended F-Gases Regulation came into effect in 2015, and has been amended several times to strengthen its provisions since then. By 2030 F-Gases Regulation is set to cut the EU’s emissions by 2/3 of 2014 levels comparitively. The implemented methods include waste management standards, reporting requirements, recovery, and uses of alternative refrigerants. 
+
+### Key Provisions
+
+Key Provisions of the F-Gases Regulation:
+
+1. A gradual phase-down on uses of F-gases, with a target of reducing their use by two-thirds by 2030 compared to 2014 levels.
+2. Banning the use of F-gases with a high global warming potential (GWP) in certain new products and equipment such as domestic refrigerators, domestic freezers, and stationary air conditioning.
+3. Stronger restrictions on uses of F-gases in particular applications such as refrigeration and air conditioning systems, and a requirement for the use of alternative substances with lower GWP.
+4. A ban on the placing on the market of certain products and equipment containing F-gases with a high GWP(Global warming potential)
+5. The introduction of leak checking, and repair requirements for equipment containing F-gases, to reduce emissions from leaks.
+
+### Mitigation
+
+### Impact
+
+### Alternatives
+
+## Future Direction & Challenges : Recovery & Disposal
+
+### Disadvantages
+
+## Conclusion
+
+
+![FIGURE 1 - HFCs PHASE-DOWN steps](https://www.fgasquota.com/images/sheme.png)
+
+![FIGURE 2 - F-GAS PHASE DOWN](https://www.gea.com/nl/binaries/gea-f-gas-phase-down_tcm26-63622.jpg)
+
+![Figure 3 - GWP](https://www.caro.ie/getattachment/Knowledge-Hub/Science-of-Climate-Change/Greenhouse-Gases/GHG-IPCC-(1).png)
+
+![Figure 4 - GWP](https://theengineeringmindset.com/wp-content/uploads/2017/09/global-warming-potential-refrigerants.gif)
+
+
+# - 
+## - 
+### - 
+#### -
+##### - 
