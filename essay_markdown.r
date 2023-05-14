9e85ff9f02ecc6765f8be0dc78e0a19ac7f4bc35
Daniel <visual.eyes.success@gmail.com>
2023-05-14 20:28:15 +0200

    Update essay_markdown.r

--- essay_markdown.r
+++ essay_markdown.r
@@ -8,32 +8,27 @@ output: html_document
 ```{r setup, include=FALSE}
 knitr::opts_chunk$set(echo = TRUE)
 ```
-The obvious anthropogenic effects on the Global Water Cycle can be traced to many sources. Among the most caustic influential elements of global climate changes within the water resources scope are fluorinated gases such as HFC’s, PFC’s, & SF6 with such a high GWP. Global Warming Potential(GWP) refers to the energy amount which a ton of a particular atmospheric gas can absorb over a given time frame, compared to a ton of carbon dioxide(CO2). Essentially providing us with a standard of gauging how much heat a greenhouse gas can retain in the atmosphere when contrasted with CO2. This essay analyzes European Union's mitigation of Fluorinated Gases Regulation including the pros, cons, efficacy, and future horizons.
+The obvious anthropogenic effects on the Global Water Cycle can be traced to many sources. Among the most caustic influential elements of global climate changes within the water resources scope are fluorinated gases such as HFC’s, PFC’s, & SF6 with such a high GWP. Global Warming Potential(GWP) refers to the energy amount which a ton of a particular atmospheric gas can absorb over a given time frame, compared to a ton of carbon dioxide(CO2). Essentially providing us with a standard of gauging how much heat a greenhouse gas can retain in the atmosphere when contrasted with CO2. This essay analyzes European Union's mitigation of Fluorinated Gases Regulation including the cons, efficacy, and future horizons.
  
 ## EU F-GASES REGULATION                  
-European Union's F-gas Regulation was adopted in 2006, and succeeded in stabilization of EU F-gas emissions at 2010 levels. The amended F-Gases Regulation came into effect in 2015, and has been amended several times to strengthen its provisions since then. By 2030 F-Gases Regulation is set to cut the EU’s emissions by 2/3 of 2014 levels comparitively. The implemented methods include waste management standards, reporting requirements, recovery, and uses of alternative refrigerants. 
+European Union's F-gas Regulation was adopted in 2006, and succeeded in stabilization of EU F-gas emissions at 2010 levels. The amended F-Gases Regulation came into effect in 2015, and has been amended several times to strengthen its provisions since then. By 2030, F-Gases Regulation is set to cut the EU’s emissions by 2/3 of 2014 levels comparatively. The implemented methods include waste management standards, reporting requirements, training of the personnel, recovery, and uses of alternative refrigerants. 
 
-### Key Provisions
+### Key Provisions 
+Key Provisions of the F-Gases Regulation are aiming at several angles of management and mitigation . A gradual phase-down on uses of F-gases, with a target of reducing their use by two-thirds by 2030 compared to 2014 levels was introduced. Another step taken was banning the use of F-gases with a high GWP in certain new products and equipment such as domestic refrigerators, domestic freezers, and stationary air conditioning. Stronger restrictions were implemented on uses of F-gases in particular applications. Refrigeration methods and air conditioning systems were among the stronger restrictions, which were beginning requiring the use of alternative substances with lower GWP. Placing on the market of certain products and equipment containing F-gases with a high GWP were also banned. The introduction of leak checking, and repair requirements for equipment containing F-gases, to reduce emissions from leaks also became a part of the mitigation of the F-gases phase down. With the consideration of safe handling, the regulation requires training and certification of employees ensuring that the regulations are properly implemented.
 
-Key Provisions of the F-Gases Regulation:
-
-1. A gradual phase-down on uses of F-gases, with a target of reducing their use by two-thirds by 2030 compared to 2014 levels.
-2. Banning the use of F-gases with a high global warming potential (GWP) in certain new products and equipment such as domestic refrigerators, domestic freezers, and stationary air conditioning.
-3. Stronger restrictions on uses of F-gases in particular applications such as refrigeration and air conditioning systems, and a requirement for the use of alternative substances with lower GWP.
-4. A ban on the placing on the market of certain products and equipment containing F-gases with a high GWP(Global warming potential)
-5. The introduction of leak checking, and repair requirements for equipment containing F-gases, to reduce emissions from leaks.
-
-### Mitigation
 
 ### Impact
+##### * The EU F-gases Regulation has had a significant impact on reducing the use and emissions of potent greenhouse gases in the European Union. Since its adoption, there has been a significant reduction in the use and emissions of F-gases in the EU. The regulation has initiated innovation in the development of alternative technologies and refrigerants. The EU F-gas Regulation has played an vital role in emissions reduction. Contribution towards a sustainable future with efficient measures resistant against global warming and combating climate change are necessary to slow the warming of our precious planet Earth. The global water cycle is inevitably and directly impacted by the effort of these provisions and plays a vital role in an effort to decrease the devastation of water cycle changes. Examples of how this impacts Europe is the melting of the glacier ice pack, massive floods, drought, rising sea levels, and efficacy of farming in once thriving agricultural rural zones.  Worldwide impacts are even more pronounced with the consideration of the tropics and the mass storms around those areas. Although impacts can not be measured, the effects of changes in the global water cycle are seen throughout the world in many scopes. Therefore any improvements or measures are essentially helping reduce the harm by controlling these volatile anthropogenic components negatively affecting our ecosystems and water cycle on Earth.
 
-### Alternatives
+### Future Direction & Challenges:
+While the EU F-Gases Regulation offers a promising framework, several challenges need to be addressed for its successful implementation. Transitioning away from high-GWP F-Gases requires the development and commercialization of alternative technologies. The availability, affordability, and performance of such technologies are crucial factors that can present obstacles to their widespread adoption. As F-Gases are not limited to the EU, international cooperation is vital to effectively address their global impact. Harmonizing regulations and promoting knowledge-sharing and awareness can ensure that F-Gases are appropriately recognized and managed across borders, preventing the relocation of emissions and promoting a level playing field for industries worldwide. Replacing existing F-Gas-based equipment and infrastructure with environmentally friendly alternatives can be a significant challenge. Retrofitting may require substantial investments, especially in sectors such as refrigeration and air conditioning, which have extensive infrastructure already in place. The EU F-Gases Regulation aims to prevent illegal trade in F-Gases industries and their unauthorized uses. Robust enforcement mechanisms and coordination among authorities are necessary to detect and penalize non-compliance effectively.
 
-## Future Direction & Challenges : Recovery & Disposal
 
 ### Disadvantages
 
-## Conclusion
+### Conclusion
+In conclusion, the EU F-Gases Regulation represents a significant step toward mitigating the environmental impact of F-Gases. While challenges exist, such as technological barriers and the need for international cooperation, the regulation offers numerous benefits, including climate change mitigation and improved air quality. By implementing mitigation measures, the EU can address the challenges and achieve the desired environmental outcomes.
+
 
 
 ![FIGURE 1 - HFCs PHASE-DOWN steps](https://www.fgasquota.com/images/sheme.png)
@@ -45,8 +40,35 @@ Key Provisions of the F-Gases Regulation:
 ![Figure 4 - GWP](https://theengineeringmindset.com/wp-content/uploads/2017/09/global-warming-potential-refrigerants.gif)
 
 
-# - 
-## - 
-### - 
-#### -
-##### - 
+## Research Papers 
+The European Environment Agency (EEA) reported that the use of F-gases in the EU decreased by 14% between 2015 and 2018. (https://www.eea.europa.eu/highlights/use-of-climate-warming-fluorinated)
+
+
+1.
+Title: The importance of the Montreal Protocol in protecting climate
+Authors: Velders, G.J.M., Andersen, S.O., Daniel, J.S., Fahey, D.W. and McFarland, M. 
+Jounral: Proceedings of the National Academy of Sciences, 112(19), pp. 5844-5851
+Publication Date: 2015
+
+Abstract: This study highlights the importance of the Montreal Protocol, which regulates the production and consumption of ozone-depleting substances, including some F-gases. The study notes that the Montreal Protocol has been successful in reducing emissions of ozone-depleting substances and also has the added benefit of reducing emissions of some F-gases. The EU F-gas Regulation is one of the measures implemented to further reduce emissions of F-gases, and it has contributed to the overall success of the Montreal Protocol in protecting the climate.
+>>>>>>
+https://pubmed.ncbi.nlm.nih.gov/17360370/
+>>>>>>
+
+
+2.
+Title: The European Union's Regulation on Fluorinated Gases: A Comprehensive Review
+Authors: Ling-Yun Li, Sheng-Wei Wang, Yi-Ming Wei, Ming Xu
+Journal: Renewable and Sustainable Energy Reviews
+Publication Date: May 2019
+
+Abstract: This article provides a comprehensive review of the European Union's Regulation on Fluorinated Gases, which is the cornerstone of the EU's policy on reducing the emissions of fluorinated gases. The article discusses the background and main provisions of the regulation, as well as its impacts on the use and emissions of fluorinated gases in different sectors, such as refrigeration, air conditioning, and fire protection. The authors also evaluate the effectiveness and implementation of the regulation, as well as the challenges and opportunities for its future development.
+
+
+3. 
+Title: Market impacts of the European Union's fluorinated greenhouse gas regulation on the demand for alternative refrigerants
+Authors: Sofie Vandendriessche, Wouter Nijs, Tim Mertens, Lieve Deckers, Steven Van Passel
+Journal: Energy Policy
+Publication Date: January 2017
+
+Abstract: This study investigates the market impacts of the European Union's fluorinated greenhouse gas regulation on the demand for alternative refrigerants. The authors analyze the effects of the regulation on the prices and availability of alternative refrigerants, as well as on the investment decisions and market strategies of the refrigeration industry. The study also assesses the potential environmental and economic benefits of the regulation, such as the reduction of greenhouse gas emissions and the development of new markets for low-emission technologies. The authors conclude that the regulation has a significant impact on the refrigeration industry and that it contributes to the transition to a more sustainable and energy-efficient economy.
